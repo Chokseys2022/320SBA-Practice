bb1e4ae1304c5fac2e3920fbae3e94aa4eb38890
Chokseys2022 <choksey2022@gmail.com>
2024-03-15 10:42:36 -0700

    2nd commit

--- src/pages/sun.jsx
+++ src/pages/sun.jsx
@@ -7,7 +7,7 @@ const Sun = () => {
     <div className="image-container">
       {sunData.map((image) => (
         <div key={image.id} className="image-item">
-          <img src={image.src} alt={image.alt} /> {/* Corrected here */}
+          <img src={sunData.png} alt={image.alt} />
           <p>{image.alt}</p>
         </div>
       ))}
